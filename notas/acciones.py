b72841c8b3fd6eeb5d42c16784d04a821380c066
Gianfranco Moirano <84589954+Gianfranco-Moirano@users.noreply.github.com>
2021-06-01 20:59:22 -0300

    Add files via upload

---
+++ notas/acciones.py
@@ -0,0 +1,47 @@
+import notas.nota as modelo
+
+class Acciones:
+
+    def crear(self, usuario):
+        print(f"\nOk, {usuario[1]}!! Vamos a crear una nueva nota")
+
+        titulo = input("Introduce el titulo de tu nota: ")
+        descripcion = input("Introduce el contenido de tu nota: ")
+
+        nota = modelo.Nota(usuario[0], titulo, descripcion)
+        guardar = nota.guardar()
+
+        if guardar[0] >= 1:
+            print(f"\n Perfecto has guardado la nota: {nota.titulo}")
+
+        else: 
+            print(f"\n No se ha guardado la nota, lo siento {usuario[1]}")
+
+
+    def mostrar(self, usuario):
+        print(f"\nVale {usuario[1]}!! Aqui tienes tus notas: ")
+
+        nota = modelo.Nota(usuario[0])
+        notas = nota.listar()
+
+        for nota in notas:
+            print("\n*************************")
+            print(nota[2])
+            print(nota[3])
+            print("\n*************************")
+
+    def borrar(self, usuario):
+        print (f"\n Vamos a borrar!!!")
+
+        titulo = input("\nIntroduce el titulo de la nota a borrar: ")
+
+        nota = modelo.Nota(usuario[0], titulo)
+        eliminar = nota.eliminar()
+
+        if eliminar[0] >= 1:
+            print(f"Hemos borrado la nota: {nota.titulo}")
+        else:
+            print("No se ha borrado la nota, intentalo de nuevo")
+ 
+
+
